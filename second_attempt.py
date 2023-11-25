f10a53dc11f378d6187b392f203ffdcf4c6ec9f8
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-11-25 15:22:25 +0000

    Second attempt working

--- second_attempt.py
+++ second_attempt.py
@@ -3,18 +3,13 @@ from typing import Iterable
 
 def get_combinations(total: int, coins: list[int]) -> Iterable[list[int]]:
     if len(coins) == 1:
-        candidates = [
-            coins * count
-            for count in range(total // coins[0] + 1)
-        ]
-        for solution in filter(lambda candidate: sum(candidate) == total, candidates):
-            yield solution
+        if total % coins[0] == 0:
+            combination = [coins[0]] * (total // coins[0])
+            yield combination
         return
-    for coin in [coin for coin in coins if coin <= total]:
-        new_coins = filter(lambda new_coin: new_coin <= coin, coins)
-        smaller_combinations = get_combinations(total - coin, list(new_coins))
-        for combo in [new_combination + [coin] for new_combination in smaller_combinations]:
-            yield combo
-        smaller_coins = filter(lambda new_coin: new_coin < coin, new_coins)
-        for combo in get_combinations(total, list(smaller_coins)):
-            yield combo
+    new_coins = coins[1::]
+    for count in range(total // coins[0] + 1):
+        new_total = total - count * coins[0]
+        for smaller_combination in get_combinations(new_total, new_coins):
+            combination = [coins[0]] * count + smaller_combination
+            yield combination
--- test_second_attempt.py
+++ test_second_attempt.py
@@ -16,14 +16,13 @@ class TestSecondAttempt(unittest.TestCase):
         result = get_combinations(6, [1, 2, 5])
 
         result_list = list(result)
-        print(result_list)
+
         assert [1, 1, 1, 1, 1, 1] in result_list
         assert [1, 1, 1, 1, 2] in result_list
         assert [1, 1, 2, 2] in result_list
-        # assert [2, 2, 2] in result_list
+        assert [2, 2, 2] in result_list
         assert [1, 5] in result_list
         self.assertEqual(len(result_list), 5)
-
 
 
 if __name__ == '__main__':
