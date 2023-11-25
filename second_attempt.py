a891d3b7fe81368e6f181318a7cf154147a444f6
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-11-25 15:49:18 +0000

    Working second attempt with no dupes

--- second_attempt.py
+++ second_attempt.py
@@ -1,18 +1,26 @@
 from typing import Iterable
 
 
-def get_combinations(total: int, coins: list[int]) -> Iterable[list[int]]:
-    if len(coins) == 1:
-        if total % coins[0] == 0:
-            combination = [coins[0]] * (total // coins[0])
-            yield combination
+def get_combinations_with_repeats(total: int, coins: list[int]) -> Iterable[list[int]]:
+    if total == 0:
+        yield []
+    if len(coins) == 0:
         return
     new_coins = coins[1::]
     for count in range(total // coins[0] + 1):
         new_total = total - count * coins[0]
-        for smaller_combination in get_combinations(new_total, new_coins):
+        for smaller_combination in get_combinations_with_repeats(new_total, new_coins):
             combination = [coins[0]] * count + smaller_combination
             yield combination
+
+
+def get_combinations(total: int, coins: list[int]) -> Iterable[list[int]]:
+    so_far = []
+    for combination in get_combinations_with_repeats(total, coins):
+        if combination in so_far:
+            continue
+        so_far.append(combination)
+        yield combination
 
 
 if __name__ == '__main__':
